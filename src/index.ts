2be56c60618c9eb78fddb377db2cde8f166fa9f0
amit-t <tiwari.m.amit@gmail.com>
2025-02-22 18:29:48 +0530

    Chore: log cleanups

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@
 import { setupApp } from '@/application/bootstrap/setup-main-app.utils';
 // Shared utilities
 import { ValidationMessages } from '@/shared/constants/validation-messages';
+import AppLogger from '@/shared/utils/app-logger.utils';
 
+const logger = new AppLogger(__filename).child({
+  filepath: __filename,
+});
 /**
  * Main application bootstrap function.
  * Initializes the application and starts the HTTP server.
@@ -12,10 +16,10 @@ async function bootstrap() {
     const app = await setupApp();
     // Start the server
     app.listen(process.env.PORT || 3000, () => {
-      console.log(`🚀 Server is running on port ${process.env.PORT || 3000}`);
+      logger.info(`🚀 Server is running on port ${process.env.PORT || 3000}`);
     });
   } catch (error) {
-    console.error(
+    logger.error(
       ValidationMessages.HTTP.INTERNAL_SERVER_ERROR,
       error instanceof Error ? error.message : 'Unknown error'
     );
@@ -24,6 +28,6 @@ async function bootstrap() {
 }
 // Start the application
 bootstrap().catch((error) => {
-  console.error('Failed to start server:', error);
+  logger.error('Failed to start server:', error);
   process.exit(1);
 });
